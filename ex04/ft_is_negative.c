f9021b666cb248a5fa1acab2756215fa92d945bc
Stephen Tee <stephentee88@gmail.com>
2025-08-07 10:44:05 +0800

    added completed questions so far

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,21 @@
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
+	}
+}
+
+int	main(void)
+{
+	return (0);
+}
---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,41 @@
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+	
+	a = '0';
+	b = a + 1;
+	c = b + 1;
+
+	while (a <= '7')
+	{
+	    b = a + 1;
+		while (b <= '8')
+		{
+		    c = b + 1;
+	        while (c <= '9')
+	        {
+                ft_putchar(a);
+                ft_putchar(b);
+                ft_putchar(c);
+                ft_putchar(',');
+                ft_putchar(' ');
+                c++;
+	        }
+	        b++;
+		}
+		a++;
+	}
+}
+
+int    main(void)
+{
+    ft_print_comb();
+    return (0);
+}
